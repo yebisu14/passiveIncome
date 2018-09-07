7dc473fd8299972937aa2f8577e31209f28b0c97
kano-hideki <kano-hideki@DMM-MAC1-0055.local>
2018-09-07 10:32:57 +0900

    購入・閲覧実装

--- prototype/onetime/README.md
+++ prototype/onetime/README.md
@@ -1,18 +1,36 @@
 # prpototype/onetime
 ワンタイムURL発行のテストコード
 
+# リクエストの種類の定義
+クライアント（動画配信、動画閲覧、動画購入デバイス）とDMMサーバが利用するリクエストの定義を行う。
+ただし、購入デバイスと閲覧デバイスは同じものになってもよい（基本的には同じものとする）
+
+#### 配信リクエスト
+主に動画配信デバイスが利用する。
+下記の処理をまとめて配信リクエストと呼ぶことにする。
+1. DMMサーバに対して、自身のウォレットアドレスを登録する。
+2. スマコンに自身のウォレットアドレス（と残高）を登録する。
+自身のウォレットアドレスを自分の識別子として利用する。
+#### 閲覧リクエスト
+主に閲覧デバイスが利用する。
+
+#### 購入リクエスト
+主に購入デバイスが利用する。
+
+
 ## client.py
 
 ## server.py
 
 
 ## URLスキーマ
-- query: http://localhost:8080/get?pk=[xxxx]
-- response: encrypted uri of movie.
+| エントリーポイント    |       意味                    | 
+|  -----             |       -----                  |
+|      /             |  現在配信中の動画一覧            |
+|  /register?uuid=[UUID] |  配信リクエストを登録する    |
 
-|key |     意味     |
-|----|     ----    |
-|pk  | ユーザの公開鍵 |
+
+
 
 ## サーバが保持するデータ
 
@@ -22,11 +40,27 @@ https://qiita.com/genzouw/items/35022fa96c120e67c637
 
 
 ### サムネイルテーブル
+static/img/thumbnails配下のサムネイルをUUIDと関連づける。
 
 ```
 CREATE TABLE thumbnails(
     uuid TEXT PRIMARY KEY NOT NULL,
-    image BLOB
+    image_uri TEXT
 )
 ```
+
+
+### ブロードキャストURIテーブル
+ウォレットアドレスと配信URLを紐づける。
+
+```
+CREATE TABLE broadcast_uris(
+    wallet_address TEXT PRIMARY KEY NOT NULL,
+    broadcast_uri TEXT
+)
+```
+
+
+# 用語の定義など
+- 購入UUID、購入番号: 購入情報を特定するためのID。購入者は購入UUIDリストを保持しており、購入UUIDがスマコン上にあることで、正当な購入者であることを証明している。
 
--- prototype/onetime/server.py
+++ prototype/onetime/server.py
@@ -1,32 +1,78 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-import hashlib
 import sqlite3
-
-app = Flask(__name__)
+from web3 import Web3, HTTPProvider
+import json
 
 DB_NAME = "./data.db"
 
-"""
-ハッシュのラッパー
-あとでアルゴリズムを変更できるようにラップしておく
-"""
-def hash(x):
-    return hashlib.sha256(x).hexdigest()
+def initContract():
+    # コントラクト初期化
+    web3 = Web3(HTTPProvider('http://localhost:9545'))
+    web3.eth.defaultAccount = web3.eth.accounts[0]
+    contractAddress = Web3.toChecksumAddress("0x057d2360abbe75f9fdf142f2cfe68cfc9a74ec12")
+    abi = '[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":false,"inputs":[{"name":"purchaseUuid","type":"string"},{"name":"deviceWalletAddress","type":"address"}],"name":"addPurchase","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"deviceWalletAddress","type":"address"}],"name":"addDevice","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"}],"name":"isBroadcastable","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"purchaseUuid","type":"string"},{"name":"walletAddress","type":"address"}],"name":"verifyPurchase","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}]'
+
+    abiJson = json.loads(abi)
+    contract = web3.eth.contract(abi = abiJson, address = contractAddress)
+    print("Contract was regulated.")
+    return contract
+
+
+
+
+
+
+app = Flask(__name__)
+
+
+
 
 """
-ユーザからの閲覧リクエストを元に、
-コントラクト上に購入履歴があるかどうかを判定、
-ある場合はvalid, ない場合はinvalidを返す。
+表示されているサムネイルをクリックされた時に呼ばれる
+閲覧リクエストに相当
+
+サーバはスマートコントラクトに対して、
+1. 購入済みチェックを行う
+2. 配信チェックを行う
+どちらかが不可能なら、invalidを返す
 """
-@app.route('/request')
-def req():
-    encrypted_uuid = request.args.get('euuid')
-    print(encrypted_uuid)
-    uuid = sk.decrypt(encrypted_uuid)
-    print(uuid)
-    return uuid
+@app.route('/watch')
+def request_watching():
+    contract = initContract()
+    uuid_purchase = request.args.get('pid')
+    wallet_address = request.args.get('addr')
+    # ブロードキャストが有効かチェック
+    if not contract.functions.isBroadcastable(wallet_address).call():
+        return "Bad device."
+    # その動画の購入履歴があるかチェック
+    if not contract.functions.verifyPurchase(uuid_purchase, wallet_address).call():
+        return "No histories of purchase."
+    # DMMのサーバ上にあるDBを見てURIを参照
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    uri = ""
+    for row in cursor.execute('SELECT * FROM broadcast_uris WHERE wallet_address=?', [wallet_address]):
+        uri = row[1]
+    return uri
+
+"""
+配信リクエストを登録する
+"""
+@app.route('/broadcast')
+def request_broadcast():
+    uri = request.args.get('uri')
+    wallet_address = request.args.get('addr')
+    # DMMのサーバ上にあるDBにURIを登録
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO broadcast_uris VALUES(?,?)', [wallet_address, uri])
+    conn.commit()
+    conn.close()
+    return "OK."
+
+
 
 
 """
@@ -38,26 +84,17 @@ def index():
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
     args = []
-    for row in cursor.execute("SELECT * FROM thumbnails"):
+    for row in cursor.execute('SELECT * FROM thumbnails'):
         args.append(
             {
                 'uri': row[1],
-                'huuid': str(hash(row[0].encode()))
+                'huuid': str(hash(row[0].encode())),
             }
         )
-
+    conn.close()
     return render_template('index.html', args = args)
 
 
-"""
-表示されているサムネイルをクリックされた時に呼ばれる
-閲覧リクエストに相当
-"""
-@app.route('/open')
-def request_watching():
-    huuid = request.args.get('huuid')
-
-    return "invalid"
 
 
 
