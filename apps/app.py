dc20c844ce99afa54275a9aecc193cf09afc56fd
kano-hideki <kano-hideki@DMM-MAC1-0055.local>
2018-09-20 14:28:49 +0900

    TOPページ修正

--- apps/app.py
+++ apps/app.py
@@ -3,11 +3,25 @@
 from flask import Flask, render_template, request
 import sqlite3
 from pathlib import Path
+from web3 import Web3, HTTPProvider
+import configparser
+import json
 
 """
 定数
 """
 DB_NAME = "./data.db"
+
+"""
+コントラクト設定
+"""
+config = configparser.ConfigParser()
+config.read('./config.ini')
+contractAddress = config.get('contract', 'contract_address')
+abi = config.get('contract', 'abi')
+abiJson = json.loads(abi)
+infra = config.get('eth', 'infra')
+
 
 app = Flask(__name__)
 
@@ -24,6 +38,22 @@ jinja_options.update({
     'comment_end_string': '#>'                  
 })                                                                               
 app.jinja_options = jinja_options    
+
+"""
+キャッシュしない設定
+"""
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 """
